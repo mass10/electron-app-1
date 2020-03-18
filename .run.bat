dfb36a351ec228e77e6fe6a785686785f262b3c3
Irisawa Masaru <mass10.github@gmail.com>
2020-03-18 10:08:01 +0900

    .

--- .run.bat
+++ .run.bat
@@ -1,5 +1,5 @@
 @REM ========== 直接実行 ==========
-@yarn electron . --aaa AAA --bbb=BBB
+@yarn electron . --aaa AAA --bbb=BBB --open-devtools
 
 @REM ========== package.json に定義した "start" スクリプトを実行 ========== 
 @REM yarn start
