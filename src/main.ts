cff0067fe1c14db189eb100f7f555eae79feb8fe
Irisawa Masaru <mass10.github@gmail.com>
2020-03-19 17:18:59 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { Application } from "./Application";
 import { Logger } from "./Logger";
+// import log from "electron-log";
 
 /**
  * エントリーポイントです。ここからアプリケーションが開始されます。
  */
 function main() {
+
+	// if (false) {
+	// 	console.log = log.log;
+	// 	log.info("### start ###");
+	// 	log.info("ロガー！！！");
+	// 	log.info("--- end ---");
+	// }
 
 	Logger.trace("<main()> called.");
 	const app = Application.getInstance();
