b0639ad49fca73e4e57bf3e52e4d52ee5ce63542
Irisawa Masaru <mass10.github@gmail.com>
2020-07-16 07:45:42 +0900

    Update .run.bat

--- .run.bat
+++ .run.bat
@@ -1,5 +1,2 @@
-@REM ========== 直接実行 ==========
-@yarn electron . --aaa AAA --bbb=BBB --open-devtools
-
-@REM ========== package.json に定義した "start" スクリプトを実行 ========== 
-@REM yarn start
+@SETLOCAL
+@CALL yarn electron . --aaa AAA --bbb=BBB --enable-logging --ignore-certificate-errors --open-devtools
