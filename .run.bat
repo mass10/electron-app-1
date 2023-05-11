343597046d6f03a605a8818087b6dec75409fe0b
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-05-11 16:33:49 +0900

    -

--- .run.bat
+++ .run.bat
@@ -1,3 +1,3 @@
 @SETLOCAL
 @CHCP 65001
-@CALL yarn electron . --aaa AAA --bbb=BBB --enable-logging --ignore-certificate-errors --open-devtools
+@CALL yarn electron . --aaa AAA --bbb=BBB --enable-logging --ignore-certificate-errors
