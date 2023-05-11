06b59472cd7ba230eed2998a7717f5c89693bc7e
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-05-11 12:21:55 +0900

    -

--- src/log/Logger.ts
+++ src/log/Logger.ts
@@ -1,5 +1,5 @@
-import { appendFileSync } from "fs";
-import { Timestamp } from "./Timestamp";
+import fs from "fs";
+import { Timestamp } from "../util/Timestamp";
 
 /**
  * ロギングクラス
@@ -21,40 +21,39 @@ export class Logger {
 	}
 
 	/**
-	 *
+	 * デバッグログを出力します。
 	 */
-	public static trace(...params: any[]): void {
-
+	public static debug(...params: any[]): void {
 		// ログファイルをローテーション
 		Logger.roateLogs();
 
 		// ロギング
-		let line = "";
-		line += Timestamp.timestamp0();
-		line += " [TRACE] ";
-		params.forEach(e => {
+		let line = `${Timestamp.timestamp0()} [DEBUG] `;
+		for (const e of params) {
 			line += e;
-		});
-		appendFileSync(".electron-app-1.log", line + "\n");
+		}
+		line += "\n";
+		fs.appendFileSync("electron-app-1.log", line);
 
 		// 標準出力にも書き込み
 		console.log(line);
 	}
 
 	/**
-	 *
+	 * エラーログを出力します。
 	 */
 	public static error(...params: any[]): void {
 		// ログファイルをローテーション
 		Logger.roateLogs();
+
 		// ロギング
-		let line = "";
-		line += Timestamp.timestamp0();
-		line += " [ERROR] ";
-		params.forEach(e => {
+		let line = `${Timestamp.timestamp0()} [ERROR] `;
+		for (const e of params) {
 			line += e;
-		});
-		appendFileSync(".electron-app-1.log", line + "\n");
+		}
+		line += "\n";
+		fs.appendFileSync("electron-app-1.log", line);
+
 		// 標準出力にも書き込み
 		console.error(line);
 	}
